d3b03cfc0f1a9a1dc0ecaaec26901283aaa808f0
GrischK <78802922+GrischK@users.noreply.github.com>
2022-01-27 22:08:46 +0100

    Ajout des fichiers

---
+++ function.js
@@ -0,0 +1,15 @@
+const progress_bars = document.
+querySelector('.progress')
+
+window.addEventListener('scroll', () => {
+
+    const {scrollTop, clientHeight}
+    document.documentElement;
+
+    const topElementToTopViewport =
+    progress_bars.getBoundingClientRect();
+
+    if(scrollTop > (scrollTop + topElementToTopViewport).tofixed() - clientHeight + 0.50) {
+      progress_bars.classList.add('active')
+    }
+})
