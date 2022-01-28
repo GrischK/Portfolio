e64d3a9e0de06c98bef1d9a992667c12ba9e9ba9
GrischK <grischka.gorski@laposte.net>
2022-01-28 20:14:48 +0100

    Ajout fonctions animations barres competences au scroll

--- function.js
+++ function.js
@@ -1,5 +1,69 @@
+/*
 window.addEventListener('scroll',() => {
   console.log('Hello');
 });
+*/
 
+const nav = document.querySelector('.progress');
 
+window.addEventListener('scroll',() =>{
+  if(window.scrollY>1400) {
+      nav.classList.add('scroll')
+  }
+});
+
+const nav1 = document.querySelector('.progress1');
+
+window.addEventListener('scroll',() =>{
+  if(window.scrollY>1430) {
+      nav1.classList.add('scroll1')
+  }
+});
+
+const nav2 = document.querySelector('.progress2');
+
+window.addEventListener('scroll',() =>{
+  if(window.scrollY>1460) {
+      nav2.classList.add('scroll2')
+  }
+});
+
+const nav3 = document.querySelector('.progress3');
+
+window.addEventListener('scroll',() =>{
+  if(window.scrollY>1490) {
+      nav3.classList.add('scroll3')
+  }
+});
+
+const nav4 = document.querySelector('.progress4');
+
+window.addEventListener('scroll',() =>{
+  if(window.scrollY>1520) {
+      nav4.classList.add('scroll4')
+  }
+});
+
+const nav5 = document.querySelector('.progress5');
+
+window.addEventListener('scroll',() =>{
+  if(window.scrollY>1550) {
+      nav5.classList.add('scroll5')
+  }
+});
+
+const nav6 = document.querySelector('.progress6');
+
+window.addEventListener('scroll',() =>{
+  if(window.scrollY>1580) {
+      nav6.classList.add('scroll6')
+  }
+});
+
+const nav7 = document.querySelector('.progress7');
+
+window.addEventListener('scroll',() =>{
+  if(window.scrollY>1610) {
+      nav7.classList.add('scroll7')
+  }
+});
